15b87fd41caa8a335aae87716397e7e03bdd0fab
viktor <vic-tol@mail.ru>
2021-02-12 11:41:52 +0300

    String [#468846]

---
+++ src/ru/job4j/Greeting.java
@@ -0,0 +1,15 @@
+package ru.job4j;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        String idea =  "I like Java!"  ;
+        String man = "But I am a newbie.";
+        int year = 2021;
+        String separate = " ";
+        String separate1 = " - ";
+        String id = idea + separate + man + separate1  + year;
+
+        System.out.println(id);
+    }
+}
