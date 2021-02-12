52593329b7d52eba46306ebd93c79aaf4148c2df
viktor <vic-tol@mail.ru>
2021-02-12 11:57:29 +0300

    String [#468846]

--- src/ru/job4j/Greeting.java
+++ src/ru/job4j/Greeting.java
@@ -4,12 +4,9 @@ public class Greeting {
 
     public static void main(String[] args) {
         String idea =  "I like Java!"  ;
-        String man = "But I am a newbie.";
         int year = 2021;
-        String separate = " ";
-        String separate1 = " - ";
-        String id = idea + separate + man + separate1  + year;
+        String separate = " - ";
 
-        System.out.println(id);
+        System.out.println(idea + separate + "But I am a newbie." + separate + year);
     }
 }
