7773f1a725578ad568e092977e43cb722101601d
viktor <vic-tol@mail.ru>
2021-02-12 12:40:20 +0300

    Площадь треугольника [#468846]

--- src/ru/job4j/SqArea.java
+++ src/ru/job4j/SqArea.java
@@ -5,13 +5,13 @@ public class SqArea {
     public static double square(int p, int k) {
         int h = p / (2*(k+1));
         int L = h*k;
-        int s = L*h;
-        double rsl = s;
-        return rsl;
+        return  L*h;
+
+
     }
 
     public static void main(String[] args) {
-        double result1 = SqArea.square(6, 2);
-        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+        double rsl = SqArea.square(6, 2);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + rsl);
     }
 }
